d73d1d7618d6b507d9a015b76a1a786a78976a4a
Albert Y. Kim <albert.ys.kim@gmail.com>
2016-04-13 11:15:30 -0400

    Added sampling file.

--- Misc/presentations.R
+++ Misc/presentations.R
@@ -1,15 +1,21 @@
 # Presentations
+library(dplyr)
+
+# Set random number generator seed value
 set.seed(76)
+
+# Ensure balanced sampling so that at least 3 each day.
 class <- c("Alison", "Aminata", "Andrew", "Carter", "Christian", "Delaney",
            "Enrique", "Jacob", "Joy", "Kyler", "Mo", "Paul", "Philip", "Shaojin")
 order <- sample(class, 12)
 last_row <- c(setdiff(class, order), "", "")
-
 order <- order %>%
   matrix(ncol=4) %>%
   rbind(sample(last_row)) %>%
   as.data.frame()
-names(order) <- paste("Day", 1:4)
+
+# Present results
+names(order) <- c("Monday 5/9", "Wednesday 5/11", "Friday 5/13", "Monday 5/16")
 order
 
 
